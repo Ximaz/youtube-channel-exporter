f2def477d44fcaded6fad0a0cef5f5fa3b671517
Ximaz <malo.durand@epitech.eu>
2023-10-19 09:56:29 +0200

    adding the skip_existing feature so videos aren't getting downloaded twice.

--- youtube_exporter/download_videos.py
+++ youtube_exporter/download_videos.py
@@ -1,3 +1,4 @@
+import os
 import threading
 import typing
 
@@ -12,20 +13,37 @@ def _batch_list(
         yield iterable[i : i + batches]
 
 
-def download_video(video_url: str, output_path: str):
+def download_video(video_url: str, output_path: str, skip_if_exists: bool = True):
     youtube_video = pytube.YouTube(url=video_url)
+
+    if skip_if_exists:
+        is_already_downloaded = any(
+            map(
+                lambda filename: filename.startswith(video.title),
+                os.listdir(output_path),
+            )
+        )
+        if is_already_downloaded:
+            return
+
     video = youtube_video.streams.get_highest_resolution()
+
     if video is None:
         print(f"Unable to download the video at '{video_url}'")
     else:
         video.download(output_path=output_path)
 
 
-def download_videos(video_urls: typing.Iterable[str], output_path: str, batches: int = 4):
+def download_videos(
+    video_urls: typing.Iterable[str],
+    output_path: str,
+    batches: int = 4,
+    skip_existing: bool = True,
+):
     threads = list(
         map(
             lambda video_url: threading.Thread(
-                target=download_video, args=(video_url, output_path)
+                target=download_video, args=(video_url, output_path, skip_existing)
             ),
             video_urls,
         )
